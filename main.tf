eeb0d41ce5c7eb124a7e2bc460f58a91e6d8229d
Adémar Prothon <ademar.prothon@gmail.com>
2025-07-08 16:37:27 -0400

    feat: testing the solution + security groups

--- main.tf
+++ main.tf
@@ -1,17 +1,13 @@
-resource "aws_subnet" "blue_orange" {
-  vpc_id     = var.vpc_id
-  cidr_block = cidrsubnet(data.aws_vpc.blue_orange.cidr_block, 8, 0) # take the first /24 range in the VPC, supposing it's available
-
-  tags = {
-    Name = "blue-orange"
-  }
-}
+##################
+# Blue resources
+#################
 
 resource "aws_instance" "blue" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   associate_public_ip_address = false
   subnet_id                   = aws_subnet.blue_orange.id
+  security_groups             = [aws_security_group.blue.name]
   tags = {
     Name = "blue"
   }
@@ -37,18 +33,63 @@ EOF
 
 }
 
+resource "aws_security_group" "blue" {
+  name   = "blue"
+  vpc_id = var.vpc_id
+  tags = {
+    Name = "blue"
+  }
+}
+
+resource "aws_ec2_instance_connect_endpoint" "blue" {
+  subnet_id          = aws_subnet.blue_orange.id
+  security_group_ids = [aws_security_group.blue.id]
+  tags = {
+    Name = "blue"
+  }
+}
+
+###############
+# Orange resources
+###############
+
 resource "aws_instance" "orange" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   associate_public_ip_address = false
   subnet_id                   = aws_subnet.blue_orange.id
   key_name                    = aws_key_pair.orange.key_name
+  security_groups             = [aws_security_group.orange.name]
   tags = {
     Name = "orange"
   }
 
 }
 
+resource "aws_security_group" "orange" {
+  name   = "orange"
+  vpc_id = var.vpc_id
+  tags = {
+    Name = "orange"
+  }
+}
+
+
+# resource "aws_vpc_security_group_ingress_rule" "orange" {
+#   security_group_id = aws_security_group.orange.id
+
+#   referenced_security_group_id = aws_security_group.blue.id
+#   ip_protocol                  = -1 # -1 means all protocols
+#   from_port                    = 22
+#   to_port                      = 22
+
+#   description = "Allow SSH traffic from blue security group"
+# }
+
+
+####################
+# Global resources
+####################
 resource "tls_private_key" "orange" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -57,4 +98,13 @@ resource "tls_private_key" "orange" {
 resource "aws_key_pair" "orange" {
   key_name   = "orange"
   public_key = tls_private_key.orange.public_key_openssh
+}
+
+resource "aws_subnet" "blue_orange" {
+  vpc_id     = var.vpc_id
+  cidr_block = cidrsubnet(data.aws_vpc.blue_orange.cidr_block, 8, 0) # take the first /24 range in the VPC, supposing it's available
+
+  tags = {
+    Name = "blue-orange"
+  }
 }
